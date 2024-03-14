eea4f9edbb7d453b0ab65e7b11daf0586dce564e
Brandich-ENI <150451535+Brandich-ENI@users.noreply.github.com>
2024-03-14 10:47:59 +0100

    Ajout se souvenir de moi (#9)
    * Ajout du se souvenir de moi avec un délai à 1 semaine
    
    * Ajout contrainte d'unicité sur pseudo comme demandé dans DescriptionProduit
    
    * Mise en place bootstrap dans form et mise en place d'un formulaire de modification de profil

--- assets/app.js
+++ assets/app.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
